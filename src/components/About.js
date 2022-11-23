656c01fcd8d9e5b0e52a1d3244d78cb8bacb6fe2
Ryan See <ryanjosephsee@gmail.com>
2022-11-22 17:37:43 -0800

    commit tree a649dc023fb87e9d20562127e4710e324fa87e32

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,16 @@
 import React from "react";
+import Links from "./Links";
 
 function About() {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <p>{props.bio}</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links github={props.links.github} linkedin={props.links.linkedin} />
     </div>
   );
 }
 
 export default About;
+
